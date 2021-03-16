186a86689318717affd5830160f8f0716a774c7f
Amy Amrani <vesamrani@gmail.com>
2021-03-16 12:16:30 -0400

    defined constant CATEGORIES for product and updated the form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+  CATEGORIES = ['Entryway', 'Living Room', 'Dining Room', 'Kitchen', 'Bedroom', 'Play Room', 'Bathroom', 'Laundry Room', 'Outdoor/Patio', 'Garage']
+
   belongs_to :user
 end
