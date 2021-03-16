ca8dc9b71b12b1e442cae88a6f5e5d1befea7f18
Amy Amrani <vesamrani@gmail.com>
2021-03-15 22:17:15 -0400

    added full_name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   belongs_to :group, optional: true
   has_many :products
+
+  def full_name
+    first_name + ' ' + last_name
+  end
 end
